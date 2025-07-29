7401270e271d823fff18c1ad7c481c691280ca68
H K Sindhu <167395914+HKSindhu@users.noreply.github.com>
2025-07-29 15:40:16 +0530

    Add files via upload

---
+++ string10.java
@@ -0,0 +1,15 @@
+package String;
+
+public class string10 {
+
+	public static void main(String[] args) {
+		String str = "Sindhu is Good!"; 
+		// Convert the above string to all lowercase. 
+		String lowerStr = str.toLowerCase(); 
+		// Display the two strings for comparison. 
+		System.out.println("Original String: " + str); 
+		System.out.println("String in lowercase: " + lowerStr);
+
+	}
+
+}
---
+++ string9.java
@@ -0,0 +1,13 @@
+package String;
+
+public class string9 {
+
+	public static void main(String[] args) {
+		String str = "Sindhu is good girl."; 
+		// Convert the above string to a char array. 
+		char[] arr = str.toCharArray(); 
+		// Display the contents of the char array. 
+		System.out.println(arr);
+	}
+
+}
