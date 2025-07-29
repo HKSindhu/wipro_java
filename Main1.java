b764d01c58a2b3e0a36d2924867b2520bf1f6e52
H K Sindhu <167395914+HKSindhu@users.noreply.github.com>
2025-07-29 14:25:42 +0530

    Add files via upload

---
+++ Main1.java
@@ -0,0 +1,12 @@
+package day3;
+
+public class Main1 {
+
+	public static void main(String[] args) {
+		Author a=new Author("Sindhu","sin@gmail.com",'F');
+		Book b=new Book("Sin",a,500.0,100);
+		System.out.println(b);
+
+	}
+
+}
